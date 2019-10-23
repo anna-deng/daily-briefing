8120b1f6ae8fc9c4584e1e9a1944a28dfc23a25b
Jonathan Dai <jonathandai1226@gmail.com>
2019-10-22 22:14:06 -0500

    calendar update

--- src/data/getCalendarEvents.js
+++ src/data/getCalendarEvents.js
@@ -4,10 +4,9 @@ const gapi_key = 'AIzaSyBR-cZfMpmV2MQk-dK7llqHDpGA619BQnY'
 const calendar_id = 'u.northwestern.edu_s49ei1nj6p02usako7uu4th260@group.calendar.google.com'
 const calendar_url = `https://www.googleapis.com/calendar/v3/calendars/${calendar_id}/events?key=${gapi_key}`
 
-export const getEvents = () => {
-  axios.get(calendar_url)
+export async function getEvents() {
+  return await axios.get(calendar_url)
   .then(response => {
-    console.log(response.data)
     return response.data
   })
 }
