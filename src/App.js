7279eeaaa72ad3b41ff1efaa08976fbeaf2fc30b
lommyti <thomasli2020@u.northwestern.edu>
2019-10-30 18:58:59 -0500

    click interaction happening but not very useful

--- src/App.js
+++ src/App.js
@@ -1,22 +1,17 @@
-import React, {useEffect, useState } from "react";
-import {
-  BrowserRouter as Router,
-  Switch,
-  Route,
-} from "react-router-dom";
-import './App.css';
-import Card from './components/Card/Card';
-import PreviewCard from './components/PreviewCard/PreviewCard'
-import './data/getCalendarEvents'
+import React, { useEffect, useState } from "react";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import "./App.css";
+import Card from "./components/Card/Card";
+import PreviewCard from "./components/PreviewCard/PreviewCard";
+import "./data/getCalendarEvents";
 import { getEvents } from "./data/getCalendarEvents";
-import * as firebase from 'firebase';
-import StyledFirebaseAuth from 'react-firebaseui/StyledFirebaseAuth';
+import * as firebase from "firebase";
+import StyledFirebaseAuth from "react-firebaseui/StyledFirebaseAuth";
 
 // Configure FirebaseUI.
- // Configure FirebaseUI.
+// Configure FirebaseUI.
 
 export default function App() {
-
   // const [isLoggedIn, setIsLooggedIn] = useState(false)
   // useEffect(() => {
   //   this.unregisterAuthObserver = firebase.auth().onAuthStateChanged(
@@ -70,82 +65,102 @@ export default function App() {
   //   );
   // }
   return (
-        <Router>
-          <div>
-            <Switch>
-              <Route path="/about">
-                <About />
-              </Route>
-              <Route path="/users">
-                <Users />
-              </Route>
-              <Route path="/">
-                <Home />
-              </Route>
-            </Switch>
-          </div>
-        </Router>
+    <Router>
+      <div>
+        <Switch>
+          <Route path="/about">
+            <About />
+          </Route>
+          <Route path="/users">
+            <Users />
+          </Route>
+          <Route path="/">
+            <Home />
+          </Route>
+        </Switch>
+      </div>
+    </Router>
   );
 }
 
 function Home() {
+  const [meetingsList, setMeetingsList] = useState(null);
+  const [updateCalendar, setupdateCalendar] = useState(false);
 
-  const [meetingsList, setMeetingsList] = useState(null)
-  const [updateCalendar, setupdateCalendar] = useState(false)
-
-  useEffect (() => {
-    var data
+  useEffect(() => {
+    var data;
     getEvents().then(response => {
-      data = response
-      setMeetingsList(data)
-      console.log(data)
-    })
-  }, [updateCalendar, meetingsList])
+      data = response;
+      setMeetingsList(data);
+      console.log(data);
+    });
+  }, [updateCalendar, meetingsList]);
 
   const sortMeetingsList = () => {
     if (meetingsList) {
-      meetingsList.items.sort(function(a,b) {
+      meetingsList.items.sort(function(a, b) {
         return new Date(a.start.dateTime) - new Date(b.start.dateTime);
-      })
+      });
       meetingsList.items = meetingsList.items.filter(function(event) {
         return new Date(event.start.dateTime) >= new Date(Date.now());
       });
     }
-  }
+  };
 
-  const renderCards = () => { 
-      sortMeetingsList()
-      return(
-        (meetingsList) &&
+  const renderCards = () => {
+    sortMeetingsList();
+    return (
+      meetingsList && (
         <Card
-          startTime={new Date(meetingsList.items[0].start.dateTime).toLocaleTimeString([], {hour: '2-digit', minute:'2-digit'})} 
-          endTime={new Date(meetingsList.items[0].end.dateTime).toLocaleTimeString([], {hour: '2-digit', minute:'2-digit'})}
+          startTime={new Date(
+            meetingsList.items[0].start.dateTime
+          ).toLocaleTimeString([], { hour: "2-digit", minute: "2-digit" })}
+          endTime={new Date(
+            meetingsList.items[0].end.dateTime
+          ).toLocaleTimeString([], { hour: "2-digit", minute: "2-digit" })}
           meetingTitle={meetingsList.items[0].summary}
           name={"Anna Deng"} // from LinkedIn
-          title={"Student at Northwestern University"} 
+          title={"Student at Northwestern University"}
           description={"let's find a time to work on 338 together..."}
           email={meetingsList.items[0].creator.email}
         />
       )
+    );
+  };
+
+  function toggleIsExpanded(isExpanded) {
+    return !isExpanded;
   }
 
   const renderPreviewCards = () => {
-    sortMeetingsList()
-    return (meetingsList) && meetingsList.items.map((event, i) => {
-        return(
-          (i !== 0) ? <PreviewCard
-          startTime={new Date(event.start.dateTime).toLocaleTimeString([], {hour: '2-digit', minute:'2-digit'})}
-          meetingTitle={event.summary}
-          /> :
-          null
-        )
-    })
-  }
+    sortMeetingsList();
+    return (
+      meetingsList &&
+      meetingsList.items.map((event, i) => {
+        var isExpanded = false;
+        console.log("before", isExpanded);
+        return i !== 0 ? (
+          <div onClick={() => alert("after", toggleIsExpanded(isExpanded))}>
+            <PreviewCard
+              startTime={new Date(event.start.dateTime).toLocaleTimeString([], {
+                hour: "2-digit",
+                minute: "2-digit"
+              })}
+              meetingTitle={event.summary}
+              isExpanded={isExpanded}
+            />
+          </div>
+        ) : null;
+      })
+    );
+  };
 
   return (
     <div>
       {/* <h1 className="App-title"> NEXT MEETING:</h1> */}
-      <button onClick={() => setupdateCalendar(!updateCalendar)}>Refresh Calendar</button> 
+      <button onClick={() => setupdateCalendar(!updateCalendar)}>
+        Refresh Calendar
+      </button>
       {renderCards()}
       {renderPreviewCards()}
     </div>
--- src/components/PreviewCard/PreviewCard.js
+++ src/components/PreviewCard/PreviewCard.js
@@ -1,16 +1,20 @@
 import React from "react";
 import "./preview-card.css";
 
-const PreviewCard = ({ startTime, meetingTitle }) => {
-  return (
-    <div className="preview-card-container">
-      <div className="preview-card-header">
-        <p>{startTime}</p>
+const PreviewCard = ({ startTime, meetingTitle, isExpanded }) => {
+  if (isExpanded) {
+    return (
+      <div className="preview-card-container">
+        <div className="preview-card-header">
+          <p>{startTime}</p>
+        </div>
+        <div className="preview-card-body">
+          <p className="preview-card-body--text">{meetingTitle}</p>
+        </div>
       </div>
-      <div className="preview-card-body">
-        <p className="preview-card-body--text">{meetingTitle}</p>
-      </div>
-    </div>
-  );
+    );
+  } else {
+    return <div className="card-container">wacka</div>;
+  }
 };
 export default PreviewCard;
