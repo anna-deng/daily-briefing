edc0e579c395670ab9b52d2220e43dfc06b412dd
Drew Parsons <andrewparsons2020@u.northwestern.edu>
2019-11-05 20:00:50 -0600

    this is semi stable??

--- src/Auth.js
+++ src/Auth.js
@@ -1,6 +1,5 @@
 import React, { Component } from 'react';
 import * as firebase from 'firebase';
-import StyledFirebaseAuth from 'react-firebaseui/StyledFirebaseAuth';
 import App from './App'
 import './GoogleLogin.css';
 
@@ -8,7 +7,7 @@ import gmail from './data/gmail';
 
 // import { gapi } from 'gapi-script';
 import { gapi, loadAuth2 } from 'gapi-script'
-
+const calendar_id = 'u.northwestern.edu_s49ei1nj6p02usako7uu4th260@group.calendar.google.com'
 
 var firebaseConfig = {
   apiKey: "AIzaSyBR-cZfMpmV2MQk-dK7llqHDpGA619BQnY",
@@ -32,146 +31,96 @@ var firebaseConfig = {
 
 firebase.initializeApp(firebaseConfig);
 firebase.analytics();
-console.log(gmail);
-gmail.listLabels();
-
-class Auth extends Component {
-  constructor(props) {
-      super(props);
-      this.state = {
-          user: null
-      }
-  }
-  async componentDidMount() {
-      let auth2 = await loadAuth2(firebaseConfig.clientId, '')
-      if (auth2.isSignedIn.get()) {
-          this.updateUser(auth2.currentUser.get())
-      } else {
-          this.attachSignin(document.getElementById('customBtn'), auth2);
-      }
-  }
-  async componentDidUpdate() {
-      if(!this.state.user) {
-          let auth2 = await loadAuth2(firebaseConfig.clientId, '')
-          this.attachSignin(document.getElementById('customBtn'), auth2);
-      }
-  }
-  updateUser(currentUser) {
-      let name = currentUser.getBasicProfile().getName()
-      console.log(name)
-      let profileImg = currentUser.getBasicProfile().getImageUrl()
-      this.setState({
-          user: {
-              name: name,
-              profileImg: profileImg
-          }
-      })
-  }
-  attachSignin(element, auth2) {
-      auth2.attachClickHandler(element, {},
-          (googleUser) => {
-              this.updateUser(googleUser);
-          }, (error) => {
-              console.log(JSON.stringify(error))
-          });
-  }
-  signOut = () => {
-      let auth2 = gapi.auth2.getAuthInstance();
-      auth2.signOut().then(() => {
-          this.setState({ user: null })
-          console.log('User signed out.');
-      });
-  }
-  render() {
-      if(this.state.user) {
-          return (
-            <App></App>
-          );
-      } else {
-          return (
-              <div className="container">
-                  <div id="customBtn" className="btn login">
-                      Login
-                  </div>
-              </div>
-          );
-      }
-  }
-}
-
-export default Auth;
-
-// const calendar_id = 'u.northwestern.edu_s49ei1nj6p02usako7uu4th260@group.calendar.google.com'
-// firebase.initializeApp(firebaseConfig);
-// firebase.analytics();
 // console.log(gmail);
 // gmail.listLabels();
 
-// export default class Auth extends React.Component {
-
-//   // The component's Local state.
-//   state = {
-//     isSignedIn: false // Local signed-in state.
-//   };
-
-//   // Configure FirebaseUI.
-//   uiConfig = {
-//     // Popup signin flow rather than redirect flow.
-//     signInFlow: 'popup',
-//     // We will display Google and Facebook as auth providers.
-//     signInOptions: [
-//       firebase.auth.GoogleAuthProvider.PROVIDER_ID,
-//     ],
-//     callbacks: {
-//       // Avoid redirects after sign-in.
-//       signInSuccessWithAuthResult: () => false
-//     }
-//   };
-
-//   // Listen to the Firebase Auth state and set the local state.
-//   async componentDidMount() {
-//     this.unregisterAuthObserver = firebase.auth().onAuthStateChanged(
-//         (user) => {
-//           this.setState({isSignedIn: !!user})
-//           gapi.client.init({
-//             apiKey: firebaseConfig.apiKey,
-//             clientId: firebaseConfig.clientID,
-//             discoveryDocs: firebaseConfig.discoveryDocs,
-//             scope: firebaseConfig.scopes.join(' '),
-//           })
-//           .then(function () {
-//             // Use Google's "apis-explorer" for research: https://developers.google.com/apis-explorer/#s/calendar/v3/
-//             // Events: list API docs: https://developers.google.com/calendar/v3/reference/events/list
-//             console.log(gapi.client.calendar.events.list({
-//               'calendarId': calendar_id,
-//               'timeZone': 'America/Chicago',
-//               'singleEvents': true,
-//               'timeMin': (new Date()).toISOString(), //gathers only events not happened yet
-//               'maxResults': 20,
-//               'orderBy': 'startTime'
-//             }));
-//           })
-//         }
-//     );
-//   }
-
-//   // Make sure we un-register Firebase observers when the component unmounts.
-//   // componentWillUnmount() {
-//   //   this.unregisterAuthObserver();
-//   // }
-
-//   render() {
-//     if (!this.state.isSignedIn) {
-//       return (
-//         <div>
-//           <h1>My App</h1>
-//           <p>Please sign-in:</p>
-//           <StyledFirebaseAuth uiConfig={this.uiConfig} firebaseAuth={firebase.auth()}/>
-//         </div>
-//       );
-//     }
-//     return (
-//       <App></App>
-//     );
-//   }
-// }
+class Auth extends Component {
+    constructor(props) {
+      super(props);
+  
+      this.state = {
+        isSignedIn: false,
+      }
+      
+    }
+  
+    componentDidMount() {
+  
+      const successCallback = this.onSuccess.bind(this);
+      
+      window.gapi.load('auth2', () => {
+        this.auth2 = gapi.auth2.init({
+          client_id: firebaseConfig.clientId,
+        })
+  
+        // this.auth2.attachClickHandler(document.querySelector('#loginButton'), {}, this.onLoginSuccessful.bind(this))
+  
+        this.auth2.then(() => {
+          console.log('on init');
+          this.setState({
+            isSignedIn: this.auth2.isSignedIn.get(),
+          });
+        });
+      });    
+  
+      window.gapi.load('signin2', function() {
+        // Method 3: render a sign in button
+        // using this method will show Signed In if the user is already signed in
+        var opts = {
+          width: 200,
+          height: 50,
+          client_id: firebaseConfig.clientId,
+          onSuccess: successCallback
+        }
+        gapi.signin2.render('loginButton', opts)
+      })
+    }
+  
+    onSuccess() {
+      console.log('on success')
+      this.setState({
+        isSignedIn: true,
+        err: null
+      })
+    }
+  
+    onLoginFailed(err) {
+      this.setState({
+        isSignedIn: false,
+        error: err,
+      })
+    }
+  
+    getContent() {
+      if (this.state.isSignedIn) {
+        return <p>hello user, you're signed in </p>
+      } else {
+        return (
+          <div>
+            <p>You are not signed in. Click here to sign in.</p>
+            <button id="loginButton">Login with Google</button>
+          </div>
+        )
+      }
+      
+    }
+    
+    render() {
+        if (this.state.isSignedIn) {
+            return(
+                <App></App>
+            )
+        }
+        else {
+            return (      
+                <div className="App">
+                  <header className="App-header">
+                    <h2>Sample App.</h2>
+                    {this.getContent()}           
+                  </header>
+                </div>
+              );
+        }
+    }
+  }
+  export default Auth;
